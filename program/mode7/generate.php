7b53a359c46882030b6f4d104348ae9d382d02b9
Matt Robinson <git@nerdoftheherd.com>
2007-07-04 08:18:37 +0000

    Define constants properly in generate.php

--- program/mode7/generate.php
+++ program/mode7/generate.php
@@ -1,14 +1,14 @@
 <?php
-	define(WHITE,'#ffffff');
-	define(RED,'#ff0000');
-	define(GREEN,'#00ff00');
-	define(BLUE,'#0000ff');
-	define(YELLOW,'#ffff00');
-	define(BLACK,'#000000');
-	define(MAGENTA,'#ff00ff');
-	define(CYAN,'#00ffff');
+	define('WHITE','#ffffff');
+	define('RED','#ff0000');
+	define('GREEN','#00ff00');
+	define('BLUE','#0000ff');
+	define('YELLOW','#ffff00');
+	define('BLACK','#000000');
+	define('MAGENTA','#ff00ff');
+	define('CYAN','#00ffff');
 	
-	define(NEW_LINE_HTML,"</tr>\r\n\t<!-- mode7 row --><tr>");
+	define('NEW_LINE_HTML',"</tr>\r\n\t<!-- mode7 row --><tr>");
 	
 	$collookup[WHITE]='w';
 	$collookup[RED]='r';
@@ -19,9 +19,9 @@
 	$collookup[MAGENTA]='m';
 	$collookup[CYAN]='c';
 
-	define(JUST_TEXT,0);
-	define(NEC_GRAPHICS,1);
-	define(FULL_GRAPHICS,2);
+	define('JUST_TEXT',0);
+	define('NEC_GRAPHICS',1);
+	define('FULL_GRAPHICS',2);
 
 	$displaymode=JUST_TEXT;
 	$charwidth=16;
