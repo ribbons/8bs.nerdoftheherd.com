97acc62017c601725dae85f8ddbf5441b49cc65d
Matt Robinson <git@nerdoftheherd.com>
2009-08-05 11:28:07 +0000

    Corrected the mapping of pound signs in the Mode 0 conversion.

--- generate/convertmode0.php
+++ generate/convertmode0.php
@@ -9,13 +9,28 @@
 			$this->html=str_replace('%includejs%', '', $this->html);
 			
 			$text=implode('',file($filename));
+			$disptext='';
 			
-			$text=str_replace('&','&amp;',$text);
-			$text=str_replace('  ','  ',$text);
-			$text=str_replace("\r","<br>\r",$text);
+			for($convert = 0; $convert < strlen($text); $convert++):
+				switch(ord($text[$convert])):
+					case 13:
+						$disptext.= "<br>\r";
+						break;
+					case 38:
+						$disptext.= '&amp;';
+						break;
+					case 96:
+						$disptext.= '£';
+						break;
+					default:
+						$disptext.= $text[$convert];
+				endswitch;
+			endfor;
+			
+			$disptext=str_replace('  ','  ',$disptext);
 			
 			$this->html.='<div class="centralcol mode0">';
-			$this->html.=$text;
+			$this->html.=$disptext;
 			$this->html.='</div>';
 			
 			$this->html.=implode('', file('pages/footer.html'));
