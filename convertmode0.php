f8de0badf80e124fa68aefacc8962e76351d81e7
Matt Robinson <git@nerdoftheherd.com>
2009-12-20 15:18:33 +0000

    Overhauled the mode 0 conversion to have code to specifically process all characters and fail on unknown chars, instead of just passing the read character to the output file except for special cases.  Also fixed < and > characters in the output, and put some temporary messages in place of handling special characters 9, 28 and 29.

--- convertmode0.php
+++ convertmode0.php
@@ -12,21 +12,90 @@
 			
 			$text=file_get_contents($filename);
 			$disptext='';
+			$row=0;
+			$column=0;
 			
 			for($convert = 0; $convert < strlen($text); $convert++):
-				switch(ord($text[$convert])):
+				$charcode = ord($text[$convert]);
+				
+				switch($charcode):
+					case 9:
+						echo "Control character? 9\n";
+						$disptext.= ' ';
+						break;
 					case 13:
-						$disptext.= "<br>\r";
+						# Line break
+						$column=79;
+						break;
+					case 28:
+						echo "Control character? 28\n";
+						$disptext.= ' ';
+						break;
+					case 29:
+						echo "Control character? 29\n";
+						$disptext.= ' ';
+						break;
+					case ($charcode >= 32 && $charcode <= 37):
+						# {space}!"#$%
+						$disptext.= $text[$convert];
 						break;
 					case 38:
 						$disptext.= '&amp;';
 						break;
+					case ($charcode >= 39 && $charcode <= 59):
+						# '()*+,-./0-9:;
+						$disptext.= $text[$convert];
+						break;
+					case 60:
+						$disptext.= '&lt;';
+						break;
+					case 61:
+						# =
+						$disptext.= $text[$convert];
+						break;
+					case 62:
+						$disptext.= '&gt;';
+						break;
+					case ($charcode >= 63 && $charcode <= 91):
+						# ?@A-Z[
+						$disptext.= $text[$convert];
+						break;
+					case 93:
+						# ]
+						$disptext.= $text[$convert];
+						break;
+					case 95:
+						# _
+						$disptext.= $text[$convert];
+						break;
 					case 96:
 						$disptext.= '£';
 						break;
-					default:
+					case ($charcode >= 97 && $charcode <= 122):
+						# a-z
 						$disptext.= $text[$convert];
+						break;
+					case 124:
+						# |
+						$disptext.= $text[$convert];
+						break;
+					case 126:
+						# ~
+						$disptext.= $text[$convert];
+						break;
+					default:
+						echo 'Unknown character value '.$charcode.' at line '.$row.' column '.$column." - aborting\n";
+						#$disptext.= ' ';
+						exit(1);
 				endswitch;
+				
+				$column++;
+				
+				if($column > 79):
+					$disptext.= "<br>\r";
+					$row++;
+					$column=0;
+				endif;
 			endfor;
 			
 			$disptext=str_replace('  ','  ',$disptext);
