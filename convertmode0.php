d9895d9eaa325cb9b7639c29d93e10ec4fabd1ea
Matt Robinson <git@nerdoftheherd.com>
2009-12-21 19:12:09 +0000

    Implemented correctly processing the ascii formatting codes 28 and 29 in the Mode 0 conversion to html code and css styling.

--- convertmode0.php
+++ convertmode0.php
@@ -11,10 +11,14 @@
 			$this->html=str_replace('%includejs%', '', $this->html);
 			
 			$text=file_get_contents($filename);
+			
 			$disptext='';
 			$thisline='';
+			
 			$row=0;
 			$column=0;
+			
+			$underline=false;
 			
 			for($convert = 0; $convert < strlen($text); $convert++):
 				$charcode = ord($text[$convert]);
@@ -41,12 +45,54 @@
 						$column=79;
 						break;
 					case 28:
-						echo "Control character? 28\n";
-						$thisline.= ' ';
+						if($underline):
+							$thisline.= '</span>';
+							$underline=false;
+						else:
+							$thisline.= '<span class="uline">';
+							$underline=true;
+						endif;
+						
 						break;
 					case 29:
-						echo "Control character? 29\n";
-						$thisline.= ' ';
+						$convert++;
+						$column--;
+						
+						switch($text[$convert]):
+							case 'B':
+								$thisline.= '<b>';
+								break;
+							case 'b':
+								$thisline.= '</b>';
+								break;
+							case 'I':
+								$thisline.= '<i>';
+								break;
+							case 'i':
+								$thisline.= '</i>';
+								break;
+							case 'S':
+								$thisline.= '<span class="super">';
+								break;
+							case 'W':
+								$thisline.= '<span class="wide">';
+								break;
+							case 'Y':
+								$thisline.= '<span class="subs">';
+								break;
+							case 's':
+							case 'w':
+							case 'y':
+								$thisline.= '</span>';
+								break;
+							case '*':
+								$thisline.= '<span class="inv">*</span>';
+								break;
+							default:
+								echo 'Unknown control character: '.$text[$convert]."\n";
+								exit(1);
+						endswitch;
+						
 						break;
 					case ($charcode >= 32 && $charcode <= 37):
 						# {space}!"#$%
