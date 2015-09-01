6933ac1c11e8fcf4d59595c1811f341788c6b4ce
Matt Robinson <git@nerdoftheherd.com>
2015-09-01 20:23:19 +0100

    Set up rubocop, fix code & tweak rules

--- Gemfile
+++ Gemfile
@@ -1,3 +1,4 @@
-source "https://rubygems.org"
+source 'https://rubygems.org'
 
 gem 'jekyll'
+gem 'rubocop', require: false
--- _plugins/index.rb
+++ _plugins/index.rb
@@ -5,44 +5,44 @@ module Jekyll
       @base = base
       @dir = dir
       @name = 'index.html'
-      
-      self.process(@name)
-      self.read_yaml(File.join(base, '_layouts'), 'issue.html')
-      self.data['issue'] = issue
-      self.data['title'] += issue['number']
+
+      process(@name)
+      read_yaml(File.join(base, '_layouts'), 'issue.html')
+      data['issue'] = issue
+      data['title'] += issue['number']
     end
   end
-  
+
   class IndexPage < Page
     def initialize(site, base, dir)
       @site = site
       @base = base
       @dir = dir
       @name = 'index.html'
-      
-      self.process(@name)
-      self.read_yaml(File.join(base, '_layouts'), 'index.html')
-      
+
+      process(@name)
+      read_yaml(File.join(base, '_layouts'), 'index.html')
+
       issues = []
-      
-      for discimg in Dir["discimgs/*"]
+
+      Dir['discimgs/*'].each do |discimg|
         issue = {}
-        
+
         issue['imagepath'] = '/' + discimg
-        issue['number'] = discimg[/\/(8BS[0-9-]+)\.dsd/,1]
-        
+        issue['number'] = discimg[%r{/(8BS[0-9-]+)\.dsd}, 1]
+
         issues << issue
-        
+
         site.pages << IssuePage.new(site, site.source, issue['number'], issue)
       end
-      
-      self.data['issues'] = issues.sort_by{ |i| i['number'] }
+
+      data['issues'] = issues.sort_by { |i| i['number'] }
     end
   end
-  
+
   class IndexPageGenerator < Generator
     safe true
-    
+
     def generate(site)
       site.pages << IndexPage.new(site, site.source, '')
     end
