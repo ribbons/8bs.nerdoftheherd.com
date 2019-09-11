7065f8c3558cd74e42ec99c445a710490237d390
Matt Robinson <git@nerdoftheherd.com>
2019-09-11 18:45:39 +0100

    Fix compile warnings on Travis and make them fatal
    C99 is the default under GCC now so no need to set this anymore -
    especially as setting it with -std=c99 disables some extensions which is
    what was causing the compilation warnings.

--- _ext/BBC/extconf.rb
+++ _ext/BBC/extconf.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 # This file is part of the 8BS Online Conversion.
-# Copyright © 2015-2018 by the authors - see the AUTHORS file for details.
+# Copyright © 2015-2019 by the authors - see the AUTHORS file for details.
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -25,7 +25,7 @@ throw 'libstdc++ is required' unless find_library('stdc++', 'main')
 throw 'libbeebimage is required' unless find_library('beebimage', 'main')
 
 # rubocop:disable Style/GlobalVars
-$CFLAGS = '-std=c99 -O3 -Wall -Wconversion -fPIC'
+$CFLAGS = '-O3 -Wall -Wconversion -Werror -fPIC'
 # rubocop:enable Style/GlobalVars
 
 create_makefile('native_filters_c')
