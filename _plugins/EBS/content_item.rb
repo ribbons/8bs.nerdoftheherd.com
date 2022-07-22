ribbons/8bs.nerdoftheherd.com

commit 99449294db0a558b77489d60af3eb40177c1c559

Start splitting ContentItem object from MenuEntry
A lot of code relating to output content handling has ended up in the
MenuEntry class.  Start moving this in to a new ContentItem class which
is passed to the content pages instead of the menu entry.

ContentItem takes a list of files instead of paths which is cleaner and
also flagged up a couple of mistakes in the YAML data files.  Fix these
and also add a check in the DfsDisc file() method to catch these issues
more easily in future.

_plugins/EBS/content_item.rb
@@ -1,0 +1,112 @@
+# frozen_string_literal: true
+
+# This file is part of the 8BS Online Conversion.
+# Copyright © 2019 by the authors - see the AUTHORS file for details.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+module EBS
+  class ContentItem < Liquid::Drop
+    def initialize(files, entry)
+      @files = files
+
+      @type = entry.type
+      @title = entry.title
+      @offsets = entry.offsets
+      @arcpaths = entry.arcpaths
+      @arcfix = entry.arcfix
+      @modes = entry.modes
+      @linkpath = entry.linkpath
+      @imagepath = entry.imagepath
+      @model = entry.model
+    end
+
+    attr_reader :type, :title, :offsets, :arcpaths, :arcfix, :modes, :files,
+                :linkpath, :model, :imagepath
+
+    def typestr
+      @type.id2name
+    end
+
+    def modelstr
+      case @model
+      when :modelb
+        'b'
+      when :master128
+        'master'
+      end
+    end
+
+    def content
+      content = []
+
+      @files.each_with_index do |file, idx|
+        if @arcpaths.nil?
+          content << if !@offsets.nil?
+                       extract_section(file.content, @offsets, idx)
+                     elsif @type == :mode7
+                       trim_scroller(file.content, file.loadaddr)
+                     else
+                       file.content
+                     end
+        else
+          archive = Archive.from_file(file, @arcfix)
+
+          @arcpaths.each do |arcpath|
+            filecontent = archive.file(arcpath)
+            content << filecontent unless filecontent.nil?
+          end
+        end
+      end
+
+      content
+    end
+
+    def bootstrap_basic
+      splitpath = if @arcpaths.nil?
+                    [':' + @files[0].side.to_s, @files[0].dir, @files[0].name]
+                  else
+                    files[0].disc.canonicalise_path(@arcpaths[0]).split('.', 3)
+                  end
+
+      basic = 'OSCLI("DRIVE ' + splitpath[0][1] + "\")\n" \
+      'OSCLI("DIR ' + splitpath[1] + "\")\n"
+
+      if @type == :basic
+        basic + 'CHAIN "' + splitpath[2] + '"'
+      else
+        basic + 'OSCLI("RUN ' + splitpath[2] + '")'
+      end
+    end
+
+    private
+
+    MODE7_SCREEN_SIZE = 25 * 40
+
+    def trim_scroller(content, loadaddr)
+      # The first four bytes are the start and end locations of the text data
+      textstart = (content.getbyte(1) << 8 | content.getbyte(0)) - loadaddr
+      textend = (content.getbyte(3) << 8 | content.getbyte(2)) -
+                loadaddr + MODE7_SCREEN_SIZE - 1
+
+      # Chop off scroller code
+      content[textstart..textend]
+    end
+
+    def extract_section(content, offsets, index)
+      offind = index * 2
+      content[offsets[offind]..offsets[offind] + offsets[offind + 1] - 1]
+    end
+  end
+end

_data/8BS14.yaml
@@ -27,7 +27,7 @@
         :offsets: [ 17152, 1000, 286, 15360, 18176, 1000 ]
       - :title: Kickstart
         :type: :run
-        :paths: [ /kickst ]
+        :paths: [ kickst ]
   - :title: Disc Magazine
     :id: 2
     :entries:

_data/8BS21.yaml
@@ -128,7 +128,7 @@
         :paths: [ ':2.Exampl2' ]
       - :title: DIRlock Utility
         :type: :run
-        :paths: [ ':2.DIRlock ']
+        :paths: [ ':2.DIRlock']
       - :title: Number Converter
         :type: :basic
         :paths: [ ':2.NumConv' ]

_layouts/content_basic.html
@@ -1,6 +1,6 @@
 ---
 # This file is part of the 8BS Online Conversion.
-# Copyright © 2015-2017 by the authors - see the AUTHORS file for details.
+# Copyright © 2015-2019 by the authors - see the AUTHORS file for details.
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -19,16 +19,16 @@ layout:     main
 includejs:  '/common/script/emulate.js'
 ---
 
-<div id="emulator" data-image-path="{{ page.entry.imagepath }}"
+<div id="emulator" data-image-path="{{ page.item.imagepath }}"
 
-{% if page.entry.modelstr %}
-    data-model="{{ page.entry.modelstr }}"
+{% if page.item.modelstr %}
+    data-model="{{ page.item.modelstr }}"
 {% endif %}
 
     data-action="basic"></div>
 
 <div id="content">
-    <h1>{{ page.entry.title | chomp_dot }}</h1>
+    <h1>{{ page.item.title | chomp_dot }}</h1>
 
     <p>To view an emulation of this BBC Basic program, you need to have
     <span id="need-to-have"><a href="http://enable-javascript.com/">JavaScript enabled</a></span>,

_layouts/content_basic_list.html
@@ -1,6 +1,6 @@
 ---
 # This file is part of the 8BS Online Conversion.
-# Copyright © 2015-2018 by the authors - see the AUTHORS file for details.
+# Copyright © 2015-2019 by the authors - see the AUTHORS file for details.
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -25,7 +25,7 @@ action:     Listing
     <h1>{{ page.title }}</h1>
 </div>
 
-{% for item in page.entry.content %}
+{% for item in page.item.content %}
     {% if forloop.index > 1 %}<hr>{% endif %}
     <div class="mode7">{{ item | basic_to_text | mode7_text_to_mem | mode7_mem_to_html }}</div>
 {% endfor %}

_layouts/content_bootstrap.html
@@ -1,6 +1,6 @@
 ---
 # This file is part of the 8BS Online Conversion.
-# Copyright © 2016 by the authors - see the AUTHORS file for details.
+# Copyright © 2016-2019 by the authors - see the AUTHORS file for details.
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -15,4 +15,4 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ---
-{{ page.entry.bootstrap_basic }}
+{{ page.item.bootstrap_basic }}

_layouts/content_image.html
@@ -1,6 +1,6 @@
 ---
 # This file is part of the 8BS Online Conversion.
-# Copyright © 2016 by the authors - see the AUTHORS file for details.
+# Copyright © 2016-2019 by the authors - see the AUTHORS file for details.
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -19,16 +19,16 @@ layout:     main
 ---
 
 <div id="content">
-    <h1>{{ page.entry.title | chomp_dot }}</h1>
+    <h1>{{ page.item.title | chomp_dot }}</h1>
 </div>
 
 <div class="image">
-    {% for item in page.entry.content %}
+    {% for item in page.item.content %}
         {% if forloop.index > 1 %}<hr>{% endif %}
-        {{ item | bbc_pic_to_img: page.entry.typestr, page.entry.modes[forloop.index0] }}
+        {{ item | bbc_pic_to_img: page.item.typestr, page.item.modes[forloop.index0] }}
 
-        {% if page.entry.captions %}
-            <div class="caption">{{ page.entry.captions[forloop.index0] }}</div>
+        {% if page.item.captions %}
+            <div class="caption">{{ page.item.captions[forloop.index0] }}</div>
         {% endif %}
     {% endfor %}
 </div>

_layouts/content_mode0.html
@@ -1,6 +1,6 @@
 ---
 # This file is part of the 8BS Online Conversion.
-# Copyright © 2015-2018 by the authors - see the AUTHORS file for details.
+# Copyright © 2015-2019 by the authors - see the AUTHORS file for details.
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -18,7 +18,7 @@
 layout:     main
 ---
 
-{% for item in page.entry.content %}
+{% for item in page.item.content %}
     {% if forloop.index > 1 %}<hr>{% endif %}
     <div class="mode0">{{ item | mode0_to_html }}</div>
 {% endfor %}

commit e03c360d7fca61567a6280b21c90ddbbe8c2f19a

Copy captions from menu entry to content item
Although image captions were defined in the data files, these were not
being displayed in the image content pages.  This was due to the
content_image layout checking the ContentItem for a defined 'captions'
property but this never being pulled through from the menu item.

It looks like this issue was introduced in 99449294db as part of
splitting off ContentItem from MenuEntry.

_plugins/EBS/content_item.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-# Copyright © 2019-2021 Matt Robinson
+# Copyright © 2019-2022 Matt Robinson
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
 
@@ -18,11 +18,13 @@ module EBS
       @path = path
       @imagepath = parent.imagepath
       @model = entry&.model
+      @captions = entry&.captions
 
       @type = entry&.type || files[0][-1].type
     end
 
-    attr_reader :type, :title, :offsets, :modes, :files, :path, :model
+    attr_reader :type, :title, :offsets, :modes, :files, :path, :model,
+                :captions
     attr_accessor :imagepath
 
     def typestr